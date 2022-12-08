342b073e5c559c0a43ca0acca0aa8ebff548165a
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-12-08 12:54:01 -0500

    Front end clean slate

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { DarkModeContextProvider } from "./context/darkModeContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <DarkModeContextProvider>
-      <App />
-    </DarkModeContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );
+
+
