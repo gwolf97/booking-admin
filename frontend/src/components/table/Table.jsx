95f66982ec99eea9cfa7a129cb4ae7f852f64597
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-12-16 12:37:22 -0500

    Booking data on dashboard

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -6,91 +6,46 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import useFetch from "../../hooks/useFetch";
+import moment from "moment"
+import { useEffect, useState } from "react";
 
 const List = () => {
+  const {data:bookings} = useFetch("/bookings")
+
+
+
+const readableDates = (dates) => {
+  const firstDate = new Date(dates[0]).toLocaleDateString()
+  const secondDate = new Date(dates[dates.length - 1]).toLocaleDateString()
+
+  return `${firstDate}-${secondDate}`
+}
+
   const rows = [
-    {
-      id: 1143155,
-      product: "Acer Nitro 5",
-      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 785,
-      method: "Cash on Delivery",
-      status: "Approved",
-    },
-    {
-      id: 2235235,
-      product: "Playstation 5",
-      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Michael Doe",
-      date: "1 March",
-      amount: 900,
-      method: "Online Payment",
-      status: "Pending",
-    },
-    {
-      id: 2342353,
-      product: "Redragon S101",
-      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 35,
-      method: "Cash on Delivery",
-      status: "Pending",
-    },
-    {
-      id: 2357741,
-      product: "Razer Blade 15",
-      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Jane Smith",
-      date: "1 March",
-      amount: 920,
-      method: "Online",
-      status: "Approved",
-    },
-    {
-      id: 2342355,
-      product: "ASUS ROG Strix",
-      img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Harold Carol",
-      date: "1 March",
-      amount: 2000,
-      method: "Online",
-      status: "Pending",
-    },
   ];
   return (
     <TableContainer component={Paper} className="table">
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell className="tableCell">Tracking ID</TableCell>
-            <TableCell className="tableCell">Product</TableCell>
-            <TableCell className="tableCell">Customer</TableCell>
-            <TableCell className="tableCell">Date</TableCell>
-            <TableCell className="tableCell">Amount</TableCell>
-            <TableCell className="tableCell">Payment Method</TableCell>
-            <TableCell className="tableCell">Status</TableCell>
+            <TableCell className="tableCell">Booking ID</TableCell>
+            <TableCell className="tableCell">User</TableCell>
+            <TableCell className="tableCell">Hotel Name</TableCell>
+            <TableCell className="tableCell">Hotel ID</TableCell>
+            <TableCell className="tableCell">Dates</TableCell>
+            <TableCell className="tableCell">Created At</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell className="tableCell">{row.id}</TableCell>
-              <TableCell className="tableCell">
-                <div className="cellWrapper">
-                  <img src={row.img} alt="" className="image" />
-                  {row.product}
-                </div>
-              </TableCell>
-              <TableCell className="tableCell">{row.customer}</TableCell>
-              <TableCell className="tableCell">{row.date}</TableCell>
-              <TableCell className="tableCell">{row.amount}</TableCell>
-              <TableCell className="tableCell">{row.method}</TableCell>
-              <TableCell className="tableCell">
-                <span className={`status ${row.status}`}>{row.status}</span>
-              </TableCell>
+          {bookings.map((booking) => (
+            <TableRow key={booking._id}>
+              <TableCell className="tableCell">{booking._id}</TableCell>
+              <TableCell className="tableCell">{booking.user}</TableCell>
+              <TableCell className="tableCell">{booking.hotel.name}</TableCell>
+              <TableCell className="tableCell">{booking.hotel.id}</TableCell>
+              <TableCell className="tableCell">{readableDates(booking.dates)}</TableCell>
+              <TableCell className="tableCell">{moment(booking.createdAt).format("MMMM Do YY, h:mm:ssa")}</TableCell>
             </TableRow>
           ))}
         </TableBody>
